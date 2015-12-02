fa8de0726534038450da3d0b6f27037557a54910
Rudi Prihandoko <toko44a@gmail.com>
2015-12-02 15:29:05 +0700

    first commit

---
+++ laravel/one/resources/views/pages/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="id">
+<head>
+	<meta charset="UTF-8">
+	<title>Tentang Kami</title>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<h1>
+	Tentang Kami
+</h1>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vehicula diam a ligula aliquet elementum. Suspendisse non justo sit amet tortor sagittis venenatis laoreet sit amet tellus. Quisque facilisis, sapien vitae iaculis luctus, enim tellus accumsan lorem, nec scelerisque nisi lacus at lorem. Quisque pharetra nibh nec justo tristique, sed tempus diam sollicitudin. Phasellus sit amet justo fermentum, consequat lorem sed, sagittis lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam ut lacus condimentum turpis eleifend volutpat et sed ligula. 
+</p>
+<p>
+	My name is {{ $name }}
+</p>
+</body>
+</html>
